edf6a35dc776bcf88426ee0a70d7dd31ca91f704
Nadav Samet <thesamet@gmail.com>
2016-11-11 21:17:07 -0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ set -e
 echo Copying sources
 rsync --filter=':- .gitignore' -r . ec2-user@demo.thesamet.com:proto-demo
 echo Compiling...
-ssh ec2-user@demo.thesamet.com -C "cd proto-demo; sbt compile server/stage; ./run.sh"
+ssh -t ec2-user@demo.thesamet.com -C "./session.sh"
 
